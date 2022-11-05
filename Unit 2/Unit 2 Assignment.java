18e6bf3df55f01a6df2235664629faec06fc51cf
RyanjXu <117485040+RyanjXu@users.noreply.github.com>
2022-11-05 10:01:22 -0400

    Update Unit 2 Assignment.java

--- Unit 2/Unit 2 Assignment.java
+++ Unit 2/Unit 2 Assignment.java
@@ -2,40 +2,39 @@ import java.util.Scanner;
 
 public class numberGuessingGame 
 {
+	private static final int secretNumber = 13;
 	public static void main(String[] args)
 	{
 		// Global Variables
 		Scanner sc = new Scanner(System.in);
-		int secretNumber = 13;
-		int guess;
 		
 		// Run Code
 		for(int i = 0; i<10; i++)
 		{
 			System.out.print("Guess the secret number? ");
-			guess = sc.nextInt();
+			int guess = sc.nextInt();
 			
-			if(guess == secretNumber) 
+			if(guess == secretNumber) // Case 1: Correct Guess
 			{
 				System.out.println("Congratulations! That is the secret number!");
 				break;
 			}
-			else if(guess < 1 || guess > 999)
+			else if(guess < 1 || guess > 999) // Case 2: Invalid input, guess is returned
 			{
 				System.out.println("Invalid Input! Ensure your number is between 1 and 999!");
 				System.out.printf("You have %d guesses left.\n", 10 - i);
 				i--;
 			}
-			else if(guess != secretNumber && i == 9)
+			else if(guess != secretNumber && i == 9) // Case 3: Final guess incorrect, game over
 			{
 				System.out.println("Too bad! Try harder next time!");
 				break;
 			}
-			else if(guess < secretNumber && guess > 0)
+			else if(guess < secretNumber && guess > 0) // Case 4: Smaller number chosen
 			{
 				System.out.printf("Choose a larger number! You have %d guesses left.\n", 9 - i);
 			}
-			else if(guess > secretNumber && guess < 1000)
+			else if(guess > secretNumber && guess < 1000) // Case 5: Larger number chosen
 			{
 				System.out.printf("Choose a smaller number! You have %d guesses left.\n", 9 - i);
 			}
